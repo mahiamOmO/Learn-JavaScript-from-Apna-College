2721e6dd5812b2bb5a1759a37ea270a0209531d3
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-01-16 19:59:39 +0600

    added loop file

---
+++ Lecture-3 Loops & Strings/never end loop.js
@@ -0,0 +1,4 @@
+//Print 1 to 5
+for(let i = 1;i>=0;i++){
+    console.log("i = ",i);
+}
---
+++ Lecture-3 Loops & Strings/sum 1 to n.js
@@ -0,0 +1,9 @@
+//Calculate 1 to 100
+
+let sum = 0;
+let n = 100;
+for(let i =1;i<= n;i++){
+    sum = sum+i;
+}
+console.log("sum= ",sum);
+console.log("loop has ended");

3bf899bcedf6f3d0a58031f33c515538be392b93
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-01-16 12:54:19 +0600

    added again

---
+++ Lecture-2 Operator & Conditions/assignmentoperator.js
@@ -0,0 +1,10 @@
+//Assignment operator
+let a = 5;
+let b = 2;
+
+a+=4; //a = a+4
+
+console.log("a = ",a);
+
+//all operator
+//+=4  *= , %=4 , **=4 
--- Lecture-2 Operator & Conditions/unaryoperator.js
+++ Lecture-2 Operator & Conditions/unaryoperator.js
@@ -6,10 +6,13 @@ let b = 2;
 
 console.log("a = "," & b = ",b);
 
-a  = a + 1;  //shortcut a++
-a = a -1;    //shortcut a--
-console.log(a);
+//a  = a + 1;  //shortcut a++
+//a = a -1;    //shortcut a--
+//console.log(a);
 
+
+console.log("--a = ",--a);
+console.log("a = ",a);
 
 
 

6c24f25dc78b2ace12bb115e4da23bc7e1ae92f5
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-01-16 22:03:45 +0600

    strings all file

---
+++ Lecture-3 Loops & Strings/string2.js
@@ -0,0 +1,13 @@
+let obj = {
+    item: "pen",
+    price: 10,
+};
+
+let output = `the cost of ${obj.item} is ${obj.price} ruupes`;
+console.log(output);
+
+console.log("This cost of",obj.item," is",obj.price,"rupees");
+
+//Template literals
+let specialString = `This is a template literal`;
+console.log(typeof specialString);

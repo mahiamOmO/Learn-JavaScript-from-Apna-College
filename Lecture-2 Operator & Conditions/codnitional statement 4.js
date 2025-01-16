41e0aa17f068b7d84a4703394d2d50603ab164c8
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-01-16 13:43:14 +0600

    conditinal statement another file

---
+++ Lecture-2 Operator & Conditions/codnitional statement 4.js
@@ -0,0 +1,18 @@
+//Conditional statement
+
+let mode = "dark";
+let color;
+
+if(mode == "black"){
+    color = "black";
+}else if(mode === "blue"){
+    color = "blue";
+
+}else if (mode === "pink"){
+
+} else {
+    color = "white";
+}
+
+console.log(color);
+

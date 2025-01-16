54e6d918e5e465dbdb58d5a970a01c80963dc624
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-01-16 13:00:12 +0600

    added again

---
+++ Lecture-2 Operator & Conditions/comparisonoperator.js
@@ -0,0 +1,17 @@
+//Comparison Operator
+
+//let a = 5;
+//let b = 2;
+
+//console.log("a == b",a ==b);  //false
+
+// != 
+
+let a = 5;
+let b = 2;
+console.log("a != b",a !=b);
+
+
+// === strict version
+
+// !==  

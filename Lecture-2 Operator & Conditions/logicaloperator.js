8494f62cf99abb6aeff9135487423eba145077a9
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-01-16 13:22:06 +0600

    added logical operator file

---
+++ Lecture-2 Operator & Conditions/logicaloperator.js
@@ -0,0 +1,12 @@
+//Logical Operator
+
+let a = 6;
+let b = 5;
+
+let cond1 = a > b;
+let cond2 = a ===6;
+//console.log("cond1 && cond2 =",cond1 && cond2);
+
+//console.log("cond1 || cond2 =",a < b || a === 6);
+ console.log("!(6<5) = ",!(a < b)); // true
+

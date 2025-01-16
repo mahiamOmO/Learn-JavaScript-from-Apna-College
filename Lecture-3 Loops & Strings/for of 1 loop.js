38b2edc6f29de4fa3aa933289e0e550c62f865e0
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-01-16 20:14:18 +0600

    for of loops

---
+++ Lecture-3 Loops & Strings/for of 1 loop.js
@@ -0,0 +1,12 @@
+// for of loop
+
+let str = "Javascript";
+
+let size = 0;
+
+for(let i of str){
+    console.log("i= ",i);
+    size++;
+}
+
+console.log("string size= ",size);
---
+++ Lecture-3 Loops & Strings/for of loop.js
@@ -0,0 +1,7 @@
+// for of loop
+
+let str = "Apna College";
+
+for(let i of str){
+    console.log("i= ",i);
+}

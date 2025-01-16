199eb07c9c31a042263c8983ea3b20977d409904
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-01-16 19:50:36 +0600

    loop files

---
+++ Lecture-3 Loops & Strings/loop.js
@@ -0,0 +1,6 @@
+//Print 1 to 5
+for(let count=1; count<=5; count++){
+    console.log("Apna College");
+}
+
+console.log("loop has ended");
---
+++ Lecture-3 Loops & Strings/sum loop.js
@@ -0,0 +1,9 @@
+//Calculate sum of 1 to 5
+
+let sum = 0;
+for(let i = 1;i <= 5; i++){
+    sum = sum+i;
+}
+
+console.log("sum= ",sum);
+console.log("loop has ended");

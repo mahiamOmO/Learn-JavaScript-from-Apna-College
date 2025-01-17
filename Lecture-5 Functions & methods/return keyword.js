a82834774fb9c935f87935e958f6bc90bb6e3927
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-01-17 21:28:23 +0600

    added function files

---
+++ Lecture-5 Functions & methods/return keyword.js
@@ -0,0 +1,8 @@
+// Function -> 2 numbers, sum
+function sum(x, y) {
+    s = x + y; // Calculates the sum
+    return s; // Returns the result
+}
+
+let val = sum(3, 4); 
+console.log(val); 
---
+++ Lecture-5 Functions & methods/sum func.js
@@ -0,0 +1,7 @@
+// Function -> 2 numbers, sum
+function sum(x, y) {
+    console.log(x + y); // This prints the sum of x and y
+}
+
+// Example function call
+sum(5, 10); // Outputs: 15

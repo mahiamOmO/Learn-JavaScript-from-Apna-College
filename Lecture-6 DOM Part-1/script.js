f2fb77d911fd1db8b079a05877cd6d426d5eee2f
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-01-17 23:00:55 +0600

    dom all files added

--- Lecture-6 DOM Part-1/script.js
+++ Lecture-6 DOM Part-1/script.js
@@ -4,3 +4,13 @@ console.dir(document.body);
 console.log(document.body);
 
 console.log(document.head);
+
+document.body.childNodes[3].innerText =  "abcd";
+
+
+let heading = document.getElementById("heading");//h1 
+console.log("heading");
+
+let headings = document.getElementsByClassName("heading-class");
+console.dir(headings);
+console.log(headings);

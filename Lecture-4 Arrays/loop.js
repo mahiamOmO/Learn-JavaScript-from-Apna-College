c92e538bf3c6dbf3e4f1e684f807a9bffce9d157
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-01-17 14:38:51 +0600

    added loop files

---
+++ Lecture-4 Arrays/loop.js
@@ -0,0 +1,9 @@
+let cities = ["delhi","pune","mumbai","agra","hyderabad"];
+
+//for(let city of cities){
+    //console.log(city);
+//}
+
+for(let city of cities){
+    console.log(city.toUpperCase());
+}
---
+++ Lecture-4 Arrays/looping over arrays.js
@@ -0,0 +1,11 @@
+let heroes = ["ironman","hulk","superman","spiderman"];
+//for loop 
+//for(let i =0; i<heroes.length; i++){
+    //console.log(heroes[i]);
+//}
+
+
+//for of
+for(let el of heroes) {
+    console.log(el);
+}

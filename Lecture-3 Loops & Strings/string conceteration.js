4154b64f8372cb4039c23fadd4774d88e377efd7
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-01-17 14:08:23 +0600

    string concateration file added

---
+++ Lecture-3 Loops & Strings/string conceteration.js
@@ -0,0 +1,8 @@
+let str1 = "apna";
+let str2 = "college";
+
+//let res = str1 + str2;
+//let res = str1.concat(str2);
+//let res = "I am learning coding from" + str1 + str2;
+let res = "hello" + 123;
+console.log(res);

da364799f8a5e3660630281e21181552b69b8362
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-01-21 19:36:57 +0600

    added style.css

---
+++ Lecture-7 DOM Part-2/script2.js
@@ -0,0 +1,9 @@
+let div = document.querySelector("div");
+
+div.style.backgroundColor = "green";
+div.style.backgroundColor = "purple";
+div.style.visibility = "hidden";
+
+div.style.fontSize = "26px";
+
+div.innerText = "Hello!";

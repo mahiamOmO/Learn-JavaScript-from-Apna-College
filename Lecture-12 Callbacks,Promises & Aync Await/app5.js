5049abb4f6d4ce77c7f746f8fb9edee37e29d74e
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-01-25 21:55:09 +0600

    added nesting

--- Lecture-12 Callbacks,Promises & Aync Await/app5.js
+++ Lecture-12 Callbacks,Promises & Aync Await/app5.js
@@ -1,3 +1,13 @@
 //nesting
 
-let
+let age = 19;
+if(age>=18) {
+    if(age>=60){
+        console.log("senior");
+    }else{
+        console.log("middle");
+    }
+
+}else{
+    console.log("child");
+}
